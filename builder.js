59927c3a6f10a1ce0c74bbc72aebfdc52073915c
Qiaonan <geonann@gmail.com>
2016-02-12 11:49:38 -0500

    finish following tutorial

--- builder.js
+++ builder.js
@@ -4,14 +4,20 @@ var Builder = require('systemjs-builder');
 // optional constructor options
 // sets the baseURL and loads the configuration file
 var builder = new Builder('./');
-builder.config({
-packages: {
-app: {
-format: 'register',
-defaultExtension: 'js'
-}
-}
-});
+builder.config(
+	{
+        bundles:{
+          'node_modules/angular2/bundles/angular2.dev.js':['angular2/core','angular2/platform/browser'],
+          'node_modules/angular2/bundles/router.dev.js':['angular2/router']
+        } ,
+        packages: {
+          app: {
+            format: 'register',
+            defaultExtension: 'js'
+          }
+        }
+      }
+);
 
 builder
 .bundle('app/main.js', 'dist/main-min.js',{ minify: true, sourceMaps: true })
