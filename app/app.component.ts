30aae1c3463d36d1929cbc9d208c6ea7dffae453
Qiaonan <geonann@gmail.com>
2016-02-12 16:48:29 -0500

    reset

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import {Component} from 'angular2/core';
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
@@ -83,3 +84,50 @@ var HEROES: Hero[] = [
   { "id": 19, "name": "Magma" },
   { "id": 20, "name": "Tornado" }
 ];
+=======
+import { Component }       from 'angular2/core';
+import { HeroService }     from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent }        from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+
+@Component({
+  selector: 'my-app',
+  styleUrls: ['app/app.component.css'],
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+    <a [routerLink]="['Dashboard']">Dashboard</a>
+    <a [routerLink]="['Heroes']">Heroes</a>
+  </nav>
+    <router-outlet></router-outlet>
+  `,
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+    ROUTER_PROVIDERS,
+    HeroService
+  ]
+})
+ @RouteConfig([
+    {
+      path: '/heroes',
+      name: 'Heroes',
+      component: HeroesComponent
+    },
+    {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+    },
+    {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
+  },
+  ])
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
+>>>>>>> master
